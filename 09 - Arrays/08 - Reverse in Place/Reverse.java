19cc3c80c656648953a507b4c7d5448f7af60f2f
Aryan Patil <patil.aryan0204@gmail.com>
2026-01-30 12:09:18 +0530

    Implemented Reverse in Place

---
+++ 09 - Arrays/08 - Reverse in Place/Reverse.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Reverse {
+    public static void main(String args[]) {
+
+        try (Scanner sc = new Scanner(System.in)) {
+            
+            System.out.print("\n Enter number of array elements: ");
+            if (!sc.hasNextInt()) return;
+            
+            int size = sc.nextInt();
+            if (size < 1) return;
+
+            int A[] = new int[size];
+
+            System.out.println("Enter " + size + " array elements:");
+            for (int i = 0; i < size; i++) {
+                if (!sc.hasNextInt()) return;
+                A[i] = sc.nextInt();
+            }
+
+            System.out.print("\n Original array: ");
+            for (int i = 0; i < A.length; i++) {
+                System.out.print(A[i] + (i < A.length - 1 ? ", " : ""));
+            }
+
+            for (int i = 0; i < size / 2; i++) {
+                int temp = A[i];
+                A[i] = A[size - 1 - i];
+                A[size - 1 - i] = temp;
+            }
+
+            System.out.print("\n Reversed array: ");
+            for (int i = 0; i < A.length; i++) {
+                System.out.print(A[i] + (i < A.length - 1 ? ", " : ""));
+            }
+            
+        }
+    }
+}
